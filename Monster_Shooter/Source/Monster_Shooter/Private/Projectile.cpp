c8516a009388422de2b989b7a840c3727b07a144
Erce Altun <57298922+ercealtun@users.noreply.github.com>
2023-05-16 21:40:04 +0300

    The projectile class

---
+++ Monster_Shooter/Source/Monster_Shooter/Private/Projectile.cpp
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Projectile.h"
+
+#include "Components/SphereComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+
+// #include "Enemy.h"
+
+
+// Sets default values
+AProjectile::AProjectile()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	CollisionSphere = CreateDefaultSubobject<USphereComponent>(TEXT("Sphere Collision"));
+	CollisionSphere->InitSphereRadius(20.f);
+
+	RootComponent = CollisionSphere;
+
+	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("Projectile Movement"));
+	ProjectileMovement->UpdatedComponent = CollisionSphere;
+	ProjectileMovement->InitialSpeed = 3000.f;
+	ProjectileMovement->MaxSpeed  = 3000.f;
+	ProjectileMovement->bRotationFollowsVelocity = true; // Projectile will rotate according to velocity
+	ProjectileMovement->bShouldBounce = true;
+
+	// If we don't hit anything after 3 seconds, the projectile will be destroyed
+	InitialLifeSpan = 3.f; 
+	
+	
+
+}
+
+// Called when the game starts or when spawned
+void AProjectile::BeginPlay()
+{
+	Super::BeginPlay();
+
+	CollisionSphere->OnComponentBeginOverlap.AddDynamic(this, &AProjectile::OnHit);
+	
+}
+
+// Called every frame
+void AProjectile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex, bool bFromSweep, const FHitResult& Hit)
+{
+	
+}
+
